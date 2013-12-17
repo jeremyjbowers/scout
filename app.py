40ca63cf16c83de9df4d7dd2d8d90dd940199b76
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-12-16 22:35:50 -0500

    Add Wazer Wine API.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+import csv
 import datetime
 import json
 
-from flask import Flask, render_template
-from pymongo import MongoClient, DESCENDING, ASCENDING
+from flask import Flask
+from pymongo import MongoClient
+import requests
 
 app = Flask("soundbites")
 
@@ -12,7 +14,28 @@ app = Flask("soundbites")
 client = MongoClient()
 db = client.apps
 scouting_reports = db.scouting_reports
+wazer_wine = db.wazer_wine
 
+@app.route('/wine/wazer.json')
+def wazer_json():
+    from flask import request
+    q = request.args.get('q', None)
+
+    query = wazer_wine.find()
+    if q:
+        query = wazer_wine.find(json.loads(q))
+
+    response = {}
+    response['items'] = []
+    start = datetime.datetime.now()
+    for item in query:
+        del(item['_id'])
+        response['items'].append(item)
+    response['meta'] = {}
+    response['meta']['count'] = len(response['items'])
+    elapsed = datetime.datetime.now() - start
+    response['meta']['time_elapsed'] = float('%s.%s' % (elapsed.seconds, elapsed.microseconds))
+    return json.dumps(response)
 
 @app.route('/scout/raw/')
 def raw_find():
--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
+import csv
 import time
 
 from bs4 import BeautifulSoup
-from pymongo import MongoClient, DESCENDING, ASCENDING
+from pymongo import MongoClient, DESCENDING
 import requests
 
 BASE_URL = 'http://scouts.baseballhall.org/adsearch'
@@ -17,82 +18,102 @@ scouting_reports.create_index([
     ('scout.last_name', DESCENDING),
     ('player.state', DESCENDING)
 ])
+wazer_wine = db.wazer_wine
+wazer_wine.create_index([
+    ('location', DESCENDING),
+    ('producer', DESCENDING),
+    ('year', DESCENDING),
+    ('classification', DESCENDING),
+    ('wine_type', DESCENDING)
+])
+
+def load_wines():
+    wazer_wine.remove()
+    r = requests.get('https://docs.google.com/spreadsheet/pub?key=0AkYn1TPmL58XdDBUMUJabDdueXVWcnNzM2NwQVkxV1E&output=csv')
+    with open('wazer.csv', 'wb') as writefile:
+        writefile.write(r.content)
+
+    with open('wazer.csv', 'rb') as readfile:
+        wazer_reader = csv.DictReader(readfile)
+        for row in wazer_reader:
+            wazer_wine.insert(row)
+
 
 def load_reports():
-	borked = 0
-	updated = 0
-	empty_reports = scouting_reports.find({"report_url": None})
-	s = requests.Session()
-	for report in empty_reports:
-		report_url = 'http://scouts.baseballhall.org/%s' % report['link'].split('&playerid=')[0]
-		time.sleep(1)
-		print report_url
-		r = s.get(report_url)
-		if r.status_code != 200:
-			borked += 1
-			print '-'
-		else:
-			soup = BeautifulSoup(r.content)
-			report['report_url'] = 'http://scouts.baseballhall.org%s' % soup.select('#content img')[0]['src']
-			unique_dict = {'report_id': report['report_id']}
-			scouting_reports.update(unique_dict, report, upsert=True, multi=False)
-			updated += 1
-			print '+'
+    borked = 0
+    updated = 0
+    empty_reports = scouting_reports.find({"report_url": None})
+    s = requests.Session()
+    for report in empty_reports:
+        report_url = 'http://scouts.baseballhall.org/%s' % report['link'].split('&playerid=')[0]
+        time.sleep(1)
+        print report_url
+        r = s.get(report_url)
+        if r.status_code != 200:
+            borked += 1
+            print '-'
+        else:
+            soup = BeautifulSoup(r.content)
+            report['report_url'] = 'http://scouts.baseballhall.org%s' % soup.select('#content img')[0]['src']
+            unique_dict = {'report_id': report['report_id']}
+            scouting_reports.update(unique_dict, report, upsert=True, multi=False)
+            updated += 1
+            print '+'
 
-	print "Updated: %s\nBorked: %s" % (updated, borked)
+    print "Updated: %s\nBorked: %s" % (updated, borked)
 
 
 def load_players():
-	borked = 0
-	updated = 0
-	for state in STATES:
-		if scouting_reports.find({"player.state": state}).count() == 0:
-			payload = {
-				'playername': '',
-				'scoutname': '',
-				'birthloc': '',
-				'birthstate': state,
-				'school': '',
-				'form_id': '619303',
-				'submit': 'Search',
-			}
-			time.sleep(5)
-			print '%s.' % state
+    borked = 0
+    updated = 0
+    for state in STATES:
+        if scouting_reports.find({"player.state": state}).count() == 0:
+            payload = {
+                'playername': '',
+                'scoutname': '',
+                'birthloc': '',
+                'birthstate': state,
+                'school': '',
+                'form_id': '619303',
+                'submit': 'Search',
+            }
+            time.sleep(5)
+            print '%s.' % state
 
-			r = requests.post(BASE_URL, data=payload)
-			if r.status_code != 200:
-				print '-'
-				borked += 1
-			else:
-				soup = BeautifulSoup(r.content)
-				results = soup.select('table.results tr')[1:]
+            r = requests.post(BASE_URL, data=payload)
+            if r.status_code != 200:
+                print '-'
+                borked += 1
+            else:
+                soup = BeautifulSoup(r.content)
+                results = soup.select('table.results tr')[1:]
 
-				for row in results:
-					report_dict = {}
-					report_dict['link'] = row.select('td a')[0]['href']
-					report_dict['report_id'] = row.select('td a')[0]['href'].split('reportid=')[1].split('&')[0]
-					report_dict['year'] = None
-					report_dict['report_url'] = None
-					try:
-						report_dict['year'] = int(row.select('td a')[2].string)
-					except:
-						pass
+                for row in results:
+                    report_dict = {}
+                    report_dict['link'] = row.select('td a')[0]['href']
+                    report_dict['report_id'] = row.select('td a')[0]['href'].split('reportid=')[1].split('&')[0]
+                    report_dict['year'] = None
+                    report_dict['report_url'] = None
+                    try:
+                        report_dict['year'] = int(row.select('td a')[2].string)
+                    except:
+                        pass
 
-					report_dict['player'] = {}
-					report_dict['player']['first_name'] = row.select('td a')[0].string.split(', ')[1]
-					report_dict['player']['last_name'] = row.select('td a')[0].string.split(', ')[0]
-					report_dict['player']['player_id'] = row.select('td a')[0]['href'].split('playerid=')[1]
-					report_dict['player']['state'] = state
+                    report_dict['player'] = {}
+                    report_dict['player']['first_name'] = row.select('td a')[0].string.split(', ')[1]
+                    report_dict['player']['last_name'] = row.select('td a')[0].string.split(', ')[0]
+                    report_dict['player']['player_id'] = row.select('td a')[0]['href'].split('playerid=')[1]
+                    report_dict['player']['state'] = state
 
-					unique_dict = {'report_id': report_dict['report_id']}
-					scouting_reports.update(unique_dict, report_dict, upsert=True, multi=False)
+                    unique_dict = {'report_id': report_dict['report_id']}
+                    scouting_reports.update(unique_dict, report_dict, upsert=True, multi=False)
 
-					print '> %s (%s): %s %s (%s)' % (
-						report_dict['report_id'], 
-						report_dict['year'],
-						report_dict['player']['first_name'],
-						report_dict['player']['last_name'],
-						report_dict['player']['state']
-					)
-				updated += 1
-	print "Updated: %s\nBorked: %s" % (updated, borked)
+                    print '> %s (%s): %s %s (%s)' % (
+                        report_dict['report_id'],
+                        report_dict['year'],
+                        report_dict['player']['first_name'],
+                        report_dict['player']['last_name'],
+                        report_dict['player']['state']
+                    )
+                updated += 1
+    print "Updated: %s\nBorked: %s" % (updated, borked)
